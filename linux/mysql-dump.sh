85aa51ab272b4b37cf4a484dd4fa9f4d73c95e89
rio-ke <kendanicrio@gmail.com>
2022-12-11 00:33:39 +0530

    updated mysql-dump-shell-sript

--- linux/mysql-dump.sh
+++ linux/mysql-dump.sh
@@ -1,32 +1,39 @@
-#!/bin/sh
-PATH=/usr/local/bin:/usr/local/sbin:~/bin:/usr/bin:/bin:/usr/sbin:/sbin
+#!/usr/bin/env bash
 
+set -x
 
-# (1) set up all the mysqldump variables
-FILE=database.sql.`date +"%d%m%Y"`
+read -p "Enter the backup file name : " FILE
+read -p "Enter the databse name : " DATABASE
+read -p "Enter the user-name : " USER
+read -p "Enter the password : " PASS
+
+echo "$FILE"
 #DBSERVER=myserver.com
-DATABASE=demo
-USER=root
-PASS=MyNewPass
+echo "$DATABASE"
+echo "$USER"
+echo "$PASS"
 
-# (2) in case you run this more than once a day, remove the previous version of the file
-unalias rm     2> /dev/null
-rm ${FILE}     2> /dev/null
-rm ${FILE}.gz  2> /dev/null
+#remove the previous version of the file if it's already copied
+unalias rm 2>/dev/null
+rm ${FILE} 2>/dev/null
+rm ${FILE}.gz 2>/dev/null
 
-# (3) do the mysql database backup (dump)
+#mysql database backup (dump)
 
 # use this command for a database server on a separate host:
-mysqldump --opt --protocol=TCP --user=${USER} --password=${PASS} --host=${DBSERVER} ${DATABASE} > ${FILE}
+#mysqldump --opt --protocol=TCP --user=${USER} --password=${PASS} --host=${DBSERVER} ${DATABASE} > ${FILE}
 
 # use this command for a database server on localhost. add other options if need be.
-#mysqldump --opt --user=${USER} --password=${PASS} ${DATABASE} > ${FILE}
+sudo mysqldump --user=${USER} --password=${PASS} ${DATABASE} >$HOME/${FILE}
 
 # (4) gzip the mysql database dump file
 gzip $FILE
 
 # (5) show the user the result
 echo "${FILE}.gz was created:"
-ls -l ${FILE}.gz
+ls -ltrh /$HOME/${FILE}.gz
+
+##[error] ls: cannot access '//home/rcms-lap-173/new-sql.gz': No such file or directory
+#but the backup file will be in /home/user/ directry 
 
 # 0 */12   * * *   root   cd /etc/mysql/backup/ && sh backupReleaseShell.sh
