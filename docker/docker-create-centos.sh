f1ca2837f3978218ebbac6898f25054c5c4cd9eb
rio-ke <88568938+rio-ke@users.noreply.github.com>
2023-04-11 12:23:34 +0530

    Update docker-create-centos.sh

--- docker/docker-create-centos.sh
+++ docker/docker-create-centos.sh
@@ -32,3 +32,26 @@ vim
 wget
 which
 yum"
+
+
+rpm -q yum-utils || yum install -y yum-utils
+
+mkdir -p $TARGET_DIR
+mkdir -p $TARGET_DIR/etc
+mkdir -p "$TARGET_DIR/var/lib/rpm"
+
+cp /etc/resolv.conf ${TARGET_DIR}/etc/
+
+mkdir -p -m 755 "$TARGET_DIR"/dev
+mknod -m 600 "$TARGET_DIR"/dev/console c 5 1
+mknod -m 600 "$TARGET_DIR"/dev/initctl p
+mknod -m 666 "$TARGET_DIR"/dev/full c 1 7
+mknod -m 666 "$TARGET_DIR"/dev/null c 1 3
+mknod -m 666 "$TARGET_DIR"/dev/ptmx c 5 2
+mknod -m 666 "$TARGET_DIR"/dev/random c 1 8
+mknod -m 666 "$TARGET_DIR"/dev/tty c 5 0
+mknod -m 666 "$TARGET_DIR"/dev/tty0 c 4 0
+mknod -m 666 "$TARGET_DIR"/dev/urandom c 1 9
+mknod -m 666 "$TARGET_DIR"/dev/zero c 1 5
+
+rpm --root "$TARGET_DIR" --initdb
