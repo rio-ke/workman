83dbaaf2c48456eac80ba49df75af8c0b4bd25cc
rio-ke <88568938+rio-ke@users.noreply.github.com>
2023-04-11 10:56:46 +0530

    Update docker-create-centos.sh

--- docker/docker-create-centos.sh
+++ docker/docker-create-centos.sh
@@ -1,1 +1,12 @@
+#!/bin/bash -ue
 
+if [ $(whoami) != 'root' ]; then
+  echo "ERR: You must be root to run this script"
+  exit 1
+fi
+
+OS_VER=7
+
+DISTRIB="centos"
+
+TARGET_DIR="/var/tmp/${DISTRIB}${OS_VER}"
